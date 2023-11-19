48bbf70d131cdbe532b9e4a8aa9651e96b5a6ab2
Restyled.io <commits@restyled.io>
2023-11-19 04:38:12 +0000

    Restyled by shellharden

--- examples/gradescope/compile.sh
+++ examples/gradescope/compile.sh
@@ -11,6 +11,6 @@ mkdir -p classes
 # Compile all java files in src directory
 java_files=$(find src -name "*.java")
 echo "compiling java files..."
-javac -cp lib/*:. -d classes "${java_files}"
+javac -cp lib/*:. -d classes "$java_files"
 echo "DONE"
 echo "--------------------------------------------------"
